3287e44540cc0a24a9858027860c3ab8e3175ae8
Sophie_G <rosaulula@gmail.com>
2024-09-18 12:08:53 -0400

    Task 2 complete

--- My project/Assets/Scripts/Controllers/Player.cs
+++ My project/Assets/Scripts/Controllers/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Codice.Client.BaseCommands;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,6 +12,18 @@ public class Player : MonoBehaviour
 
     private Vector3 velocity = Vector3.right * 0.001f;
 
+    private float accelerationTime = 5f;
+    private float maxSpeed = 15f;
+    private float acceleration;
+    private float defaultSpeed = 6f;
+    private float speed;
+    
+    void Start()
+    {
+        speed = defaultSpeed;
+        acceleration = maxSpeed / accelerationTime;
+    }
+
     void Update()
     {
         PlayerMovement();
@@ -18,24 +31,39 @@ public class Player : MonoBehaviour
 
     void PlayerMovement()
     {
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += Vector3.left * 0.01f;
+            transform.position += Vector3.left * speed * Time.deltaTime;
+            speed += acceleration * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += Vector3.right * 0.01f;
+            transform.position += Vector3.right * speed * Time.deltaTime;
+            speed += acceleration * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += Vector3.up * 0.01f;
+            transform.position += Vector3.up * speed * Time.deltaTime;
+            speed += acceleration * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += Vector3.down * 0.01f;
+            transform.position += Vector3.down * speed * Time.deltaTime;
+            speed += acceleration * Time.deltaTime;
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            speed = defaultSpeed;
+        }
+
+        if (speed >= maxSpeed)
+        {
+            speed = maxSpeed;
         }
 
     }
