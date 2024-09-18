41356918b7449db0ffb03bf9217371d8728b14fe
Sophie_G <rosaulula@gmail.com>
2024-09-18 11:13:20 -0400

    Player Controller Task
    Task 1 (?) complete

--- My project/Assets/Scripts/Controllers/Player.cs
+++ My project/Assets/Scripts/Controllers/Player.cs
@@ -9,8 +9,34 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    private Vector3 velocity = Vector3.right * 0.001f;
+
     void Update()
     {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * 0.01f;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * 0.01f;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * 0.01f;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * 0.01f;
+        }
 
     }
 
