9eb75fff47a4e10b299de8ab3a83ecb71b8c267e
Sophie_G <rosaulula@gmail.com>
2024-09-18 23:52:33 -0400

    Task 4 beginning

--- My project/Assets/Scripts/Controllers/Stars.cs
+++ My project/Assets/Scripts/Controllers/Stars.cs
@@ -6,6 +6,10 @@ public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
+    public LineRenderer line;
+
+    public Vector3 beginning;
+    public Vector3 end;
 
     // Update is called once per frame
     void Update()
