d57329fea7c05d83de3560c2529f5f3c3897ba55
Sophie_G <rosaulula@gmail.com>
2024-09-18 22:00:54 -0400

    Task 2b completed

--- My project/Assets/Scripts/Controllers/Enemy.cs
+++ My project/Assets/Scripts/Controllers/Enemy.cs
@@ -1,10 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Codice.CM.Common;
+using System;
 
 public class Enemy : MonoBehaviour
 {
+    private Boolean up = true;
     private void Update()
     {
+        EnemyMovement();
+    }
+
+    void EnemyMovement()
+    {
+
+        if(up == false)
+        {
+            transform.position += Vector3.down * 9f * Time.deltaTime;
+        }
+        else if(up == true)
+        {
+            transform.position += Vector3.up * 9f * Time.deltaTime;
+        }
+
+        if (transform.position.y > 15f)
+        {
+            up = false;
+            print(up);
+        }
+        else if (transform.position.y < -3f)
+        {
+            up = true;
+        }
     }
 
 }
