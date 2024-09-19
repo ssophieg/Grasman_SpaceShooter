24003452387876f2c2e6b47699d2ca1e724752dd
Sophie_G <rosaulula@gmail.com>
2024-09-18 22:37:17 -0400

    Task 3

--- My project/Assets/Scripts/Controllers/Asteroid.cs
+++ My project/Assets/Scripts/Controllers/Asteroid.cs
@@ -1,3 +1,4 @@
+using Codice.CM.Common;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,29 @@ using UnityEngine;
 public class Asteroid : MonoBehaviour
 {
     public float moveSpeed;
-    public float arrivalDistance;
-    public float maxFloatDistance;
+    public float arrivalDistance = 3;
+    public float maxFloatDistance = 20;
+    private float distance;
+
+    private Vector3 pointRandom = new Vector3();
 
     // Start is called before the first frame update
     void Start()
     {
+        pointRandom = new Vector3(Random.Range(-10, maxFloatDistance), Random.Range(-10, maxFloatDistance));
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.position += pointRandom.normalized * 5f * Time.deltaTime;
+        distance = Vector3.Distance(transform.position, pointRandom);
+        Debug.DrawLine(transform.position, pointRandom, color: Color.red);
+
+        if (distance <= arrivalDistance)
+        {
+             print("caught");
+;            pointRandom = new Vector3(Random.Range(0, maxFloatDistance), Random.Range(0, maxFloatDistance));
+        }
     }
 }
